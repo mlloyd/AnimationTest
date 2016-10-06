7c293c6b3e90c80260adc45795b28e99f5c5956f
Martin Lloyd <martin.lloyd@thomsonreuters.com>
2016-10-06 10:47:45 +0100

    [ADD] Color animation and shadow.

--- AnimationTest/ViewController.swift
+++ AnimationTest/ViewController.swift
@@ -16,11 +16,13 @@ class ViewController: UIViewController {
     
     var forwards = true
     
+    var step: CGFloat = 0.01
+    
     var panGestureRecognizer: UIPanGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         self.panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
         self.view.addGestureRecognizer(self.panGestureRecognizer)
         
@@ -30,20 +32,33 @@ class ViewController: UIViewController {
         self.box.backgroundColor = UIColor.yellow
         self.view.addSubview(self.box)
         
+        self.box.layer.shadowColor = UIColor.black.cgColor
+        self.box.layer.shadowOffset = CGSize(width: 0, height: 5.0)
+        self.box.layer.shadowOpacity = 0.25
+        self.box.layer.shadowRadius = 1.0
+        
         let boxAnimation = CABasicAnimation(keyPath: "position.y")
         boxAnimation.fromValue = 0
-        boxAnimation.toValue   = -(self.box.frame.size.height - 40)
+        boxAnimation.toValue   = -(self.box.frame.size.height - 80)
         boxAnimation.duration  = 1
         boxAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        box.layer.add(boxAnimation, forKey: nil)
         
         let zAnimation = CABasicAnimation(keyPath: "zPosition")
         zAnimation.fromValue = 0
         zAnimation.toValue   = 100
         zAnimation.duration  = 1
         zAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        box.layer.add(zAnimation, forKey: nil)
         
+        
+        let alphaAnimation = CAKeyframeAnimation(keyPath: "opacity")
+        alphaAnimation.values    = [1,0.75,0.5]
+        alphaAnimation.keyTimes  = [0.25,0.5,1]
+        alphaAnimation.duration  = 1
+        alphaAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        
+        box.layer.add(alphaAnimation, forKey: nil)
+        box.layer.add(boxAnimation, forKey: nil)
+        box.layer.add(zAnimation, forKey: nil)
         
         box.layer.speed = 0.0
         box.layer.anchorPoint = CGPoint(x: 0.5, y: 0.0)
@@ -56,17 +71,17 @@ class ViewController: UIViewController {
             self.panGestureRecognizer.isEnabled = true
         }
         
-        box.layer.timeOffset = forwards ?
-            min(box.layer.timeOffset + 0.01, 1.0):
-            max(box.layer.timeOffset - 0.01, 0.0)
+        let step = CFTimeInterval(self.step)
         
-        print(box.layer.timeOffset)
+        box.layer.timeOffset = forwards ?
+            min(box.layer.timeOffset + step, 1.0):
+            max(box.layer.timeOffset - step, 0.0)
     }
     
     func pan(gestureRecognizer: UIPanGestureRecognizer) {
         
         let translation = gestureRecognizer.translation(in: gestureRecognizer.view)
-        var progress = (-translation.y / 300)
+        var progress = (-translation.y / 400)
         
         if forwards == false {
             progress = 1 + progress
@@ -92,7 +107,17 @@ class ViewController: UIViewController {
         case .ended:
             print("ended")
             
-            forwards = progress > 0.5
+            let velocity = gestureRecognizer.velocity(in: self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 400
+            print("magnitude: \(magnitude), slideMultiplier: \(slideMultiplier)")
+            
+            // 2
+            self.step = max(CGFloat(0.01 * slideMultiplier), 0.01)    //Increase for more of a slide
+            
+            print(self.step)
+            
+            forwards = progress > 0.5            
             
             self.displayLink.add(to: RunLoop.main, forMode: RunLoopMode.commonModes)
             self.panGestureRecognizer.isEnabled = false
